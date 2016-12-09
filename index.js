8ebe800d6c22af5b523f5d99a1d4d6afee6f26e1
daenuprobst <daenuprobst@gmail.com>
2016-12-09 11:30:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
-    borderColor: 'yellow',
-    cursorColor: 'yellow',
+    foregroundColor: '#ecf0f1',
+    backgroundColor: '#2c3e50',
+    borderColor: '#34495e',
+    colors: {
+      black: '#2c3e50',
+      red: '#c0392b',
+      green: '#27ae60',
+      yellow: '#f39c12',
+      blue: '#2980b9',
+      magenta: '#8e44ad',
+      cyan: '#16a085',
+      white: '#bdc3c7',
+      lightBlack: '#34495e',
+      lightRed: '#e74c3c',
+      lightGreen: '#2ecc71',
+      lightYellow: '#f1c40f',
+      lightBlue: '#3498db',
+      lightMagenta: '#9b59b6',
+      lightCyan: '#1abc9c',
+      lightWhite: '#ecf0f1'
+    },
+    padding: '6px 6px',
     css: `
       ${config.css || ''}
       .tabs_nav .tabs_list .tab_text {
-        color: yellow;
+        color: #16a085;
       }
       .tabs_nav .tabs_title {
-        color: yellow;
+        color: #16a085;
       }
     `
   });
